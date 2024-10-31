b85b54396d56a7d670dbbcc78739f2db2b83faf0
Leonel Marcos <mfjleonel@gmail.com>
2024-10-31 11:06:05 -0300

    fix: regenerate a bit of power over time

--- Assets/Scripts/Player/SlowMoPower.cs
+++ Assets/Scripts/Player/SlowMoPower.cs
@@ -10,28 +10,52 @@ public class FlashLightPower : MonoBehaviour
     [SerializeField] float drainTime = 10f;
     [SerializeField] float power;
     bool slowingDown = false;
+    Coroutine coolDownCoroutine = null;
 
     private void Start()
     {
-        power = 1;
+        power = 0;
     }
 
     void Update()
     {
+        Mathf.Clamp(power, 0, 1);
+
         if (Input.GetButtonUp("Fire2") && power > 0)
+        {
             slowingDown = !slowingDown;
-        
-        if (power <= 0 && slowingDown) slowingDown = false;
+            
+            if (coolDownCoroutine == null) 
+                coolDownCoroutine = StartCoroutine(coolDown());
+        }
+
+        if (power <= 0 && slowingDown)
+        {
+            slowingDown = false;
+            coolDownCoroutine = StartCoroutine(coolDown());
+        }
 
         if (slowingDown)
         {
             Time.timeScale = 0.5f;
             power -= 1f / drainTime * Time.deltaTime;
-            bar.fillAmount = power;
         } else
         {
             Time.timeScale = 1f;
+            
+            if (power < 0.25f && coolDownCoroutine == null)
+            {
+                power += 0.05f * Time.deltaTime;
+            }
         }
 
+        bar.fillAmount = power;
+    }
+
+    IEnumerator coolDown ()
+    {
+        yield return new WaitForSeconds(5);
+
+        coolDownCoroutine = null;
     }
 }
